2cf6df7de9bde5df983687169a56d3807f7f6dd8
Narutchai <koonfluk2015@gmail.com>
2025-06-08 13:34:12 +0700

    :monocle_face: init role tabel

---
+++ prisma/seed/role.ts
@@ -0,0 +1,27 @@
+import * as dayjs from 'dayjs';
+import { Role, PrismaClient } from 'generated/prisma';
+
+const now = dayjs().toDate();
+
+export const roles: Array<Omit<Role, 'id'>> = [
+  {
+    name: 'admin',
+    createdAt: now,
+    updatedAt: now,
+  },
+  {
+    name: 'student',
+    createdAt: now,
+    updatedAt: now,
+  },
+  {
+    name: 'professor',
+    createdAt: now,
+    updatedAt: now,
+  },
+];
+
+export const executeRole = async (prisma: PrismaClient): Promise<void> => {
+  const results = await prisma.role.createMany({ data: roles });
+  console.dir({ results });
+};
---
+++ prisma/seed/seed.ts
@@ -0,0 +1,17 @@
+import { executeRole } from './role';
+import { PrismaClient } from 'generated/prisma';
+const prisma = new PrismaClient();
+
+async function main() {
+  await executeRole(prisma);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
