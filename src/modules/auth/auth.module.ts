c8c53c160f362eb7eb808be6270594ae08c7818b
naruchai <naruchai@naruchais-MacBook-Pro.local>
2025-06-21 10:05:48 +0700

    🔐 update env

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1d' },
+      signOptions: { expiresIn: process.env.JWT_EXPIRATION },
     }),
   ],
   controllers: [AuthController],
