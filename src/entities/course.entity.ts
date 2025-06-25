075855fc38190437b01960a9d6e1852f5c3cc2ba
Narutchai <koonfluk2015@gmail.com>
2025-06-25 15:20:26 +0700

    refactor entities

--- src/entities/course.entity.ts
+++ src/entities/course.entity.ts
@@ -1,14 +1,15 @@
-import { Course } from "@prisma/client";
-import { UserEntity } from "./user.entity";
+import { Course } from '@prisma/client';
+import { UserEntity } from './user.entity';
 
 export class CourseEntity implements Course {
-    id: number
-    courseId: string
-    courseName: string
-    courseDetail: string
-    createdDate: Date
-    updatedDate: Date
-    createdBy: number
-    updatedBy: number
-    user: UserEntity
+  id: number;
+  courseId: string;
+  courseName: string;
+  courseDetail: string;
+  createdDate: Date;
+  updatedDate: Date;
+  deletedDate: Date | null;
+  createdBy: number;
+  updatedBy: number;
+  user: UserEntity;
 }
