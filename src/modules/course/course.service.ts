0ee39f63d66f60f7284a1e42633d5b6c05f7d385
Narutchai <koonfluk2015@gmail.com>
2025-06-27 13:25:42 +0700

    remove disable eslint from service

--- src/modules/course/course.service.ts
+++ src/modules/course/course.service.ts
@@ -1,27 +1,24 @@
-/* eslint-disable prettier/prettier */
-import { Injectable } from "@nestjs/common";
-import { ICourseRepository } from "src/repositories/course/course.abstract";
-import { CourseModel } from "src/models/course";
-import { CreateCourseDTO } from "./dto/create-course.dto";
+import { Injectable } from '@nestjs/common';
+import { ICourseRepository } from 'src/repositories/course/course.abstract';
+import { CourseModel } from 'src/models/course';
+import { CreateCourseDTO } from './dto/create-course.dto';
 
 @Injectable()
 export class NewsService {
-    constructor(
-        private courseRespository: ICourseRepository,
-    ) { }
+  constructor(private courseRespository: ICourseRepository) {}
 
-    async createCourse(
-        createCourse: CreateCourseDTO,
-        userId: number,
-    ): Promise<CourseModel> {
-        const data = {
-            courseId: createCourse.courseId,
-            courseName: createCourse.courseName,
-            courseDetail: createCourse.courseDetail,
-            createdBy: userId,
-            updatedBy: userId,
-        };
+  async createCourse(
+    createCourse: CreateCourseDTO,
+    userId: number,
+  ): Promise<CourseModel> {
+    const data = {
+      courseId: createCourse.courseId,
+      courseName: createCourse.courseName,
+      courseDetail: createCourse.courseDetail,
+      createdBy: userId,
+      updatedBy: userId,
+    };
 
-        return this.courseRespository.createCourse(data);
-    }
+    return this.courseRespository.createCourse(data);
+  }
 }
