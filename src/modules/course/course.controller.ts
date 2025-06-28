4f570b2c6b92f3af34188d70415719535d005fd1
V0yager2 <charnatip15@gmail.com>
2025-06-28 12:11:04 +0700

    create course controller

--- src/modules/course/course.controller.ts
+++ src/modules/course/course.controller.ts
@@ -6,24 +6,24 @@ import { Request as ExpressRequest } from 'express';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 interface AuthenticatedRequest extends ExpressRequest {
-    user: {
-        userId: number;
-        roleId: number;
-    };
+  user: {
+    userId: number;
+    roleId: number;
+  };
 }
 
 @Controller('course')
 export class CourseController {
-    constructor(private readonly courseService: CourseService) { }
+  constructor(private readonly courseService: CourseService) {}
 
-    @UseGuards(JwtAuthGuard)
-    @Post()
-    async createCourse(
-        @Body() body: CreateCourseDto,
-        @Request() req: AuthenticatedRequest,
-    ) {
-        const result = await this.courseService.createCourse(body, req.user.userId);
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async createCourse(
+    @Body() body: CreateCourseDto,
+    @Request() req: AuthenticatedRequest,
+  ) {
+    const result = await this.courseService.createCourse(body, req.user.userId);
 
-        return result;
-    }
+    return result;
+  }
 }
