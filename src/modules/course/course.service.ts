5bfe524ec28f6c9cf06d402824effa6be6d8b7d5
V0yager2 <charnatip15@gmail.com>
2025-07-01 18:36:19 +0700

    create update-course.dto and update-course.service

--- src/modules/course/course.service.ts
+++ src/modules/course/course.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { ICourseRepository } from 'src/repositories/course/course.abstract';
 import { CourseModel } from 'src/models/course';
 import { CreateCourseDto } from './dto/create-course.dto';
+import { UpdateCourseDTO } from './dto/update-course';
 
 @Injectable()
 export class CourseService {
-  constructor(private courseRespository: ICourseRepository) {}
+  constructor(private courseRespository: ICourseRepository) { }
 
   async createCourse(
     createCourse: CreateCourseDto,
@@ -20,5 +21,22 @@ export class CourseService {
     };
 
     return this.courseRespository.createCourse(data);
+  }
+
+  async updateCourse(
+    id: number,
+    updateCourse: UpdateCourseDTO,
+    userId: number,
+  ): Promise<CourseModel> {
+    const existingCourse = await this.courseRespository.getCourseById(id);
+
+    const updateData = {
+      courseId: updateCourse.courseId || existingCourse.courseId,
+      courseName: updateCourse.courseName || existingCourse.courseName,
+      courseDetail: updateCourse.courseDetail || existingCourse.courseDetail,
+      updateBy: userId,
+    };
+
+    return this.courseRespository.updateCourse(id, updateData);
   }
 }
