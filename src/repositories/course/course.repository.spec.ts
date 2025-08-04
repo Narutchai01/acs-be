90ca125321de099ad9e020a6e1eb35d855e25c4c
V0yager2 <charnatip15@gmail.com>
2025-08-04 11:50:47 +0700

    feat : add define function and getCourseById function

--- src/repositories/course/course.repository.spec.ts
+++ src/repositories/course/course.repository.spec.ts
@@ -26,6 +26,10 @@ describe('CreateCourse', () => {
         repo = module.get<ICourseRepository>(ICourseRepository);
     });
 
+    it('should be defined', () => {
+        expect(repo).toBeDefined();
+    });
+
     it('Get Course success', async () => {
         const courseId = 1;
         const courseData = {
@@ -66,5 +70,44 @@ describe('CreateCourse', () => {
         await expect(
             repo.getCourse({ page: 1, pageSize: 10, searchByTypeCourse: 'all', }),
         ).rejects.toThrow('Database error');
+    });
+
+    it('Get Course by ID success', async () => {
+        const courseId = 1;
+        const courseData = {
+            id: courseId,
+            courseId: 'CSS111',
+            credits: 3,
+            curriculumId: 1,
+            typeCourseId: 1,
+            courseNameTh: 'เอ็กพอริ่ง คอมพิวเตอร์ ไซน์',
+            courseNameEn: 'EXPLORING COMPUTER SCIENCE',
+            courseDetail: 'Introduction to computer systems and computer science. Components of a computer system. Number systems and conversion.Boolean algebras and logic circuits. Manipulation of data. Operating systems, networking and Internet. Algorithms and programming. Database systems and software engineering. Artificial Intelligence. Computer professionals, social roles, ethics and laws related to computer science. Practice of computer programming for basic problems. Data, data types, expressions and introductory programming constructs.ระบบคอมพิวเตอร์และวิทยาการคอมพิวเตอร์เบื้องต้น ส่วนประกอบของระบบคอมพิวเตอร์ ระบบเลขฐานและการแปลงฐานเลข พีชคณิตบูลีนและวงจรตรรกะ การประมวลผลข้อมูล ระบบปฏิบัติการ เครือข่าย และอินเทอร์เน็ต ขั้นตอนวิธีและการเขียนโปรแกรม ระบบฐานข้อมูลและวิศวกรรมซอฟต์แวร์ ปัญญาประดิษฐ์เบื้องต้น วิชาชีพทางคอมพิวเตอร์และบทบาททางสังคม กฎหมายและจริยธรรมเกี่ยวกับวิทยาการคอมพิวเตอร์ ฝึกเขียนโปรแกรมคอมพิวเตอร์เพื่อแก้ปัญหาระดับพื้นฐาน ข้อมูล ชนิดข้อมูล นิพจน์และโครงสร้างการเขียนโปรแกรมเบื้องต้น',
+            createdDate: new Date(),
+            updatedDate: new Date(),
+            deletedDate: null,
+            createdBy: 1,
+            updatedBy: 1,
+            PrevCourse: [1, 2, 3],
+        };
+
+        MockRepo.getCourseById.mockResolvedValue(courseData);
+
+        const result = await repo.getCourseById(courseId);
+        expect(result).toEqual(courseData);
+        expect(MockRepo.getCourseById).toHaveBeenCalledWith(courseId);
+    });
+
+    it('should throw an error if getCourseById fails', async () => {
+        const courseId = 1;
+        MockRepo.getCourseById.mockRejectedValue(new Error('Database error'));
+
+        await expect(repo.getCourseById(courseId)).rejects.toThrow('Database error');
+    });
+
+    it('should throw an error if getCourseById is called with an invalid ID', async () => {
+        const courseId = -1;
+        MockRepo.getCourseById.mockRejectedValueOnce(new Error('Invalid course ID'));
+        await expect(repo.getCourseById(courseId)).rejects.toThrow('Invalid course ID');
     });
 })
