805bb85ec8345707f335f56a12ea02d6e08d5bba
Sukum <sukumpat2547@gmail.com>
2025-08-10 13:06:32 +0700

    Professor service

--- src/modules/professor/professor.service.ts
+++ src/modules/professor/professor.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { IProfessorRepository } from 'src/repositories/professor/professor.abstract';
+import { CreateProfessorDto } from './dto/create-professor.dto';
+import { ProfessorModel } from 'src/models/professor';
 
 @Injectable()
 export class ProfessorService {
   constructor(private professorRepository: IProfessorRepository) {}
 
-  async create(data: any): Promise<any> {
-    return this.professorRepository.create(data);
-    }
+  async createProfessor(
+    createProfessor: CreateProfessorDto,
+  ): Promise<ProfessorModel> {
+    const data = {
+      userId: createProfessor.userId,
+      academicPosition: createProfessor.academicPosition,
+      majorPosition: createProfessor.majorPosition,
+      profRoom: createProfessor.profRoom,
+      fieldOfExpertise: createProfessor.fieldOfExpertise,
+    };
+
+    return this.professorRepository.createProfessor(data);
+  }
 }
