51df52c88dc7b8b51238c9efadffa092975fb3bc
SukumJS <sukumpat2547@gmail.com>
2025-08-10 21:09:12 +0700

    eslint

--- src/models/professor.ts
+++ src/models/professor.ts
@@ -1,12 +1,12 @@
 import { BaseModel } from '.';
 import { UserModel } from './user';
 
-export interface ProfessrModel extends BaseModel {
-    id: number;
-    userId: number;
-    academicPosition: number;
-    majorPosition: number;
-    profRoom: string;
-    fieldOffexpertise: string;
-    user: UserModel;
+export interface ProfesorModel extends BaseModel {
+  id: number;
+  userId: number;
+  academicPosition: number;
+  majorPosition: number;
+  profRoom: string;
+  fieldOffexpertise: string;
+  user: UserModel;
 }
