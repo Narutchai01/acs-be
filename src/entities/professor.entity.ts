c8416474cc4a32918008f0a17409b1aade9a1bd0
Sukum <sukumpat2547@gmail.com>
2025-08-12 16:41:11 +0700

    word error

--- src/entities/professor.entity.ts
+++ src/entities/professor.entity.ts
@@ -1,13 +1,13 @@
 import { Professor } from '@prisma/client';
 import { UserEntity } from './user.entity';
 
-export class ProfesserEntity implements Professor {
+export class ProfessorEntity implements Professor {
   id: number;
   userId: number;
   academicPosition: number;
   majorPosition: number;
   profRoom: string;
-  fieldOffexpertise: string;
+  fieldOfExpertise: string;
   createdDate: Date;
   updatedDate: Date;
   deletedDate: Date | null;
