024ac375f545e89d64420bba327f265d8a3d2b01
V0yager2 <charnatip15@gmail.com>
2025-08-12 19:15:17 +0700

    change : change updateProfessor  to handle user name change

--- src/modules/professor/dto/update-professor.dto.ts
+++ src/modules/professor/dto/update-professor.dto.ts
@@ -1,19 +1,47 @@
 import { BaseDto } from 'src/models/dto/base.dto';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateProfessorDto extends BaseDto {
   @ApiProperty({ required: false })
+  @IsOptional()
   userId?: number;
 
   @ApiProperty({ required: false })
+  @IsOptional()
   academicPosition?: number;
 
   @ApiProperty({ required: false })
+  @IsOptional()
   majorPosition?: number;
 
   @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
   profRoom?: string;
 
   @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
   fieldOffexpertise?: number;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  firstNameTh?: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  lastNameTh?: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  firstNameEn?: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  lastNameEn?: string;
 }
--- src/modules/professor/professor.controller.ts
+++ src/modules/professor/professor.controller.ts
@@ -6,23 +6,18 @@ import {
   Param,
   Patch,
 } from '@nestjs/common';
-import { Request as ExpressRequest } from 'express';
+import { AuthenticatedRequest } from 'src/models/auth';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { ProfessorService } from './professor.service';
 import { ProfessorFactory } from './professor.factory';
 import { UpdateProfessorDto } from './dto/update-professor.dto';
-
-interface AuthenticatedRequest extends ExpressRequest {
-  user: {
-    userId: number;
-    roleId: number;
-  };
-}
+import { UsersService } from '../users/users.service';
 
 @Controller('professor')
 export class ProfessorController {
   constructor(
     private readonly ProfessorService: ProfessorService,
+    private readonly UserService: UsersService,
     private readonly ProfessorFactory: ProfessorFactory,
   ) {}
 
@@ -33,10 +28,33 @@ export class ProfessorController {
     @Body() body: UpdateProfessorDto,
     @Request() req: AuthenticatedRequest,
   ) {
-    const IdNumber = Number(id);
+    const ProfessorIdNumber = Number(id);
+    const UserIdNumber = Number(body.userId);
+
+    const professorBody = {
+      userId: body.userId,
+      academicPosition: body.academicPosition,
+      majorPosition: body.majorPosition,
+      profRoom: body.profRoom,
+      fieldOffexpertise: body.fieldOffexpertise,
+    };
+
+    const userBody = {
+      firstNameTh: body.firstNameTh,
+      lastNameTh: body.lastNameTh,
+      firstNameEn: body.firstNameEn,
+      lastNameEn: body.lastNameEn,
+    };
+
     const result = await this.ProfessorService.updateProfessor(
-      IdNumber,
-      body,
+      ProfessorIdNumber,
+      professorBody,
+      req.user.userId,
+    );
+
+    const result = await this.UserService.updateUser(
+      UserIdNumber,
+      UserBody,
       req.user.userId,
     );
 
