d6742120d31b3d8ff557d32d174bcad77df73c3e
Sukum <sukumpat2547@gmail.com>
2025-08-13 18:32:05 +0700

    CreateProfessor Repo

--- src/repositories/professor/professor.repository.ts
+++ src/repositories/professor/professor.repository.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/provider/database/prisma/prisma.service';
 import { IProfessorRepository } from './professor.abstract';
 import { ProfessorFactory } from './professor.factory';
-import { ProfessorModel, UpdateProfessorModel } from 'src/models/professor';
+import {
+  ProfessorModel,
+  UpdateProfessorModel,
+  CreateProfessorModel,
+} from 'src/models/professor';
 
 @Injectable()
 export class ProfessorRepository implements IProfessorRepository {
@@ -10,6 +14,28 @@ export class ProfessorRepository implements IProfessorRepository {
     private readonly prisma: PrismaService,
     private readonly ProfessorFactory: ProfessorFactory,
   ) {}
+
+  async createProfessor(data: CreateProfessorModel): Promise<ProfessorModel> {
+    try {
+      const professor = await this.prisma.professor.create({
+        data,
+        include: {
+          user: true,
+        },
+      });
+      return this.ProfessorFactory.mapProfessorEntityToProfessorModel(
+        professor,
+      );
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.error('Create professor failed:', error.message);
+        throw new Error(`Unable to create professor: ${error.message}`);
+      } else {
+        console.error('Unknown error:', error);
+        throw new Error('Unable to create professor: Unknown error occurred');
+      }
+    }
+  }
 
   async updateProfessor(
     id: number,
