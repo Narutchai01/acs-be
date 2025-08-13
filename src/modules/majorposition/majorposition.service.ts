c04e7c7896b4b2da503d01dc3ed646bc4519ecf0
Sukum <sukumpat2547@gmail.com>
2025-08-13 19:19:30 +0700

    getMajorPosition Service & fix update word

---
+++ src/modules/majorposition/majorposition.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { IMajorPositionRepository } from 'src/repositories/majorposition/majorposition.repository';
+import { QueryMajorPositionDto } from './dto/get-majorposition.dto';
+import { MajorPositionModel } from 'src/models/majorposition';
+
+@Injectable()
+export class MajorPositionService {
+  constructor(private majorPositionRepository: IMajorPositionRepository) {}
+
+  async getMajorPosition(
+    query: QueryMajorPositionDto,
+  ): Promise<MajorPositionModel[]> {
+    return await this.majorPositionRepository.getMajorPosition(query);
+  }
+}
--- src/repositories/majorposition/majorposition.repository.ts
+++ src/repositories/majorposition/majorposition.repository.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
-// import { majorpositionModel } from 'src/models/majorposition';
-
+import { MajorPositionModel } from 'src/models/majorposition';
+import { QueryMajorPositionDto } from 'src/modules/majorposition/dto/get-majorposition.dto';
 @Injectable()
-export abstract class IMajorpositionRepository {
-  //   abstract getMajorposition(): Promise<any[] | Error>;
+export abstract class IMajorPositionRepository {
+  abstract getMajorPosition(
+    query: QueryMajorPositionDto,
+  ): Promise<MajorPositionModel[]>;
 }
