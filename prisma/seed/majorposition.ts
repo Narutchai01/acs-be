5bda8096c3a2e409b4cc4baab87873f71b9709ae
Sukum <sukumpat2547@gmail.com>
2025-08-13 19:39:37 +0700

    Seed majorPosition

---
+++ prisma/seed/majorposition.ts
@@ -0,0 +1,40 @@
+import { Prisma, PrismaClient } from '@prisma/client';
+
+export const majorPositions: Prisma.MajorPositionCreateInput[] = [
+  {
+    positionTh: 'ผู้ช่วยศาสตราจารย์',
+    positionEn: 'Assistant Professor',
+  },
+  {
+    positionTh: 'รองศาสตราจารย์',
+    positionEn: 'Associate Professor',
+  },
+  {
+    positionTh: 'ศาสตราจารย์',
+    positionEn: 'Professor',
+  },
+];
+
+export const executeMajorPosition = async (
+  prisma: PrismaClient,
+): Promise<void> => {
+  try {
+    for (const majorPosition of majorPositions) {
+      const majorPositionData: Prisma.MajorPositionCreateInput = majorPosition;
+      const exists = await prisma.majorPosition.findFirst({
+        where: { positionTh: majorPositionData.positionTh },
+      });
+
+      if (!exists) {
+        await prisma.majorPosition.create({ data: majorPositionData });
+        console.log(`Created major position: ${majorPositionData.positionTh}`);
+      } else {
+        console.log(`
+            Major position already exists: ${majorPositionData.positionTh}`);
+      }
+    }
+  } catch (error) {
+    console.error('Error executing major position seed:', error);
+    throw error;
+  }
+};
