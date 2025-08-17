09f54be4d7d7a8880321e6b761fdb2bbfacbba55
Narutchai <koonfluk2015@gmail.com>
2025-08-17 10:52:32 +0700

    refactor: remove created_by and updated_by fields from MajorPosition model and related files

--- src/models/majorposition.ts
+++ src/models/majorposition.ts
@@ -1,9 +1,7 @@
 import { BaseModel } from '.';
-import { UserModel } from './user';
 
 export interface MajorPositionModel extends BaseModel {
   id: number;
   positionTh: string;
   positionEn: string;
-  users: UserModel[];
 }
