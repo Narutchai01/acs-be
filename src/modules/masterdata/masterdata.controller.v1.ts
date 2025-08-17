962eee5cbe978fa38b3ced6a6362478ebb5ccd64
Narutchai <koonfluk2015@gmail.com>
2025-08-17 15:31:50 +0700

    refactor: remove unused ResponseDto import and simplify getMajorPosition return type

--- src/modules/masterdata/masterdata.controller.v1.ts
+++ src/modules/masterdata/masterdata.controller.v1.ts
@@ -2,7 +2,6 @@ import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { MasterdataService } from './masterdata.service';
 import { MasterDataFactoryV1 } from './masterdata.factory.v1';
 import { MajorPositionDto } from './dto/majorposition.v1.dto';
-import { ResponseDto } from 'src/models/dto/base.dto';
 import { success } from 'src/core/interceptors/response.helper';
 
 @Controller({
@@ -16,7 +15,7 @@ export class MasterdataControllerV1 {
   ) {}
 
   @Get('/major-positions')
-  async getMajorPosition(): Promise<ResponseDto<MajorPositionDto[]>> {
+  async getMajorPosition() {
     const majorPositions = await this.masterDataService.getMajorPosition();
     const dto =
       this.masterDataFactory.mapMajorPositionModelsToDtos(majorPositions);
