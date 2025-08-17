f09f5eb3f18e6d75bf51f76e85647fff595bdcb9
V0yager2 <charnatip15@gmail.com>
2025-08-17 23:36:15 +0700

    feat : create role controller in masterData

--- src/modules/masterdata/masterdata.service.ts
+++ src/modules/masterdata/masterdata.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { IMajorPositionRepository } from 'src/repositories/majorposition/majorposition.abstract';
 import { MajorPositionModel } from 'src/models/majorposition';
+import { IRoleRepository } from 'src/repositories/role/role.abtract';
+import { RoleModel } from 'src/models/role';
 
 @Injectable()
 export class MasterdataService {
-  constructor(private majorPositionRepository: IMajorPositionRepository) {}
+  constructor(
+    private majorPositionRepository: IMajorPositionRepository,
+    private roleRepository: IRoleRepository,
+  ) {}
 
   async getMajorPosition(): Promise<MajorPositionModel[]> {
     return await this.majorPositionRepository.get();
+  }
+
+  async getList(): Promise<RoleModel[] | Error> {
+    return await this.roleRepository.getList();
+  }
+
+  async getByName(name: string): Promise<RoleModel | Error> {
+    return await this.roleRepository.getByName(name);
   }
 }
