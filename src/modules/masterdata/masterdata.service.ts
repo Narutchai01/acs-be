a35dc7adf79bca3acdd1971eed65ae65ed57ea83
ZM <zmxidn@gmail.com>
2025-08-19 20:00:29 +0700

    feat: create Service MasterData Education-Levels

--- src/modules/masterdata/masterdata.service.ts
+++ src/modules/masterdata/masterdata.service.ts
@@ -2,10 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { IMajorPositionRepository } from 'src/repositories/majorposition/majorposition.abstract';
 import { MajorPositionModel } from 'src/models/majorposition';
 import { ITypeRepositoty } from 'src/repositories/type/type.abstact';
+import { IEducationLevelRepository } from 'src/repositories/educationlevel/educationlvel.abstract'; 
+import { EducationLevelModel } from 'src/models/educationlavel';
 
 @Injectable()
 export class MasterdataService {
-  constructor(private majorPositionRepository: IMajorPositionRepository,private typeRepository: ITypeRepositoty) {}
+  constructor(private majorPositionRepository: IMajorPositionRepository,
+              private typeRepository: ITypeRepositoty,
+              private educationLevelRepository: IEducationLevelRepository) {}
 
   async getMajorPosition(): Promise<MajorPositionModel[]> {
     return await this.majorPositionRepository.get();
@@ -13,5 +17,9 @@ export class MasterdataService {
 
   async getListType(type: string) {
     return this.typeRepository.getListTypes(type);
+  }
+
+  async getEducationLevels(): Promise<EducationLevelModel[]> {
+    return this.educationLevelRepository.getEducationLevels();
   }
 }
