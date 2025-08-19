fbe661d7e41b036fe8ed59c2caa411f67be4764a
Sukum <sukumpat2547@gmail.com>
2025-08-19 23:57:50 +0700

    Combine academic-position sevice into MasterData file

--- src/modules/masterdata/masterdata.service.ts
+++ src/modules/masterdata/masterdata.service.ts
@@ -1,12 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { IMajorPositionRepository } from 'src/repositories/majorposition/majorposition.abstract';
 import { MajorPositionModel } from 'src/models/majorposition';
-
+import { AcademicPositionModel } from 'src/models/academicposition';
+import { IAcademicPositionRepository } from 'src/repositories/academicposition/academicposition.abstract';
+import { IRoleRepository } from 'src/repositories/role/role.abtract';
+import { RoleModel } from 'src/models/role';
+import { ITypeCourseRepository } from 'src/repositories/typecourse/typecourse.abstract';
+import { TypeCourseModel } from 'src/models/course';
 @Injectable()
 export class MasterdataService {
-  constructor(private majorPositionRepository: IMajorPositionRepository) {}
+  constructor(
+    private majorPositionRepository: IMajorPositionRepository,
+    private academicPositionRepository: IAcademicPositionRepository,
+    private roleRepository: IRoleRepository,
+    private typeCourseRepository: ITypeCourseRepository,
+  ) {}
 
   async getMajorPosition(): Promise<MajorPositionModel[]> {
     return await this.majorPositionRepository.get();
+  }
+
+  async getAcademicPosition(): Promise<AcademicPositionModel[]> {
+    return this.academicPositionRepository.getAcademicPosition();
+  }
+
+  async getRole(): Promise<RoleModel[]> {
+    return await this.roleRepository.getList();
+  }
+
+  async getTypeCourse(): Promise<TypeCourseModel[]> {
+    return this.typeCourseRepository.getTypeCourse();
   }
 }
