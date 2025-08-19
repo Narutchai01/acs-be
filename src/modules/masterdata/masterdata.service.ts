0e4d26c9d683ce787222774c0751c67e5408ce7d
Narutchai <koonfluk2015@gmail.com>
2025-08-19 23:44:03 +0700

    Revert "feat: create Service MasterData TypeList"
    This reverts commit 7eab5e4abacf87f3157ad82e390c4e4929b2d89f.

--- src/modules/masterdata/masterdata.service.ts
+++ src/modules/masterdata/masterdata.service.ts
@@ -1,17 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { IMajorPositionRepository } from 'src/repositories/majorposition/majorposition.abstract';
 import { MajorPositionModel } from 'src/models/majorposition';
-import { ITypeRepositoty } from 'src/repositories/type/type.abstact';
 
 @Injectable()
 export class MasterdataService {
-  constructor(private majorPositionRepository: IMajorPositionRepository,private typeRepository: ITypeRepositoty) {}
+  constructor(private majorPositionRepository: IMajorPositionRepository) {}
 
   async getMajorPosition(): Promise<MajorPositionModel[]> {
     return await this.majorPositionRepository.get();
-  }
-
-  async getListType(type: string) {
-    return this.typeRepository.getListTypes(type);
   }
 }
