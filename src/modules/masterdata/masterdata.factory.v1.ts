85e86fddf36a9d4eed114bcc522103f2052bc305
Sukum <sukumpat2547@gmail.com>
2025-08-19 23:54:29 +0700

    fix error

--- src/modules/masterdata/masterdata.factory.v1.ts
+++ src/modules/masterdata/masterdata.factory.v1.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { MajorPositionModel } from 'src/models/majorposition';
 import { MajorPositionDto } from './dto/majorposition.v1.dto';
-import { ListTypeModel } from 'src/models/type';
-import { ListTypeDto } from './dto/typelist.v1.dto';
 import { RoleModel } from 'src/models/role';
 import { RoleDto } from './dto/role.v1.dto';
-import { EducationLevelModel } from 'src/models/educationlavel';
-import { EducationLevelDto } from './dto/educationlevel.v1.dto';
-import { TypeDto } from './dto/type.v1.dto';
-import { TypeModel } from 'src/models/type';
 import { TypeCourseModel } from 'src/models/course';
 import { TypeCourseDto } from './dto/typecourse.v1.dto';
+import { ListTypeModel } from 'src/models/type';
+import { ListTypeDto } from './dto/typelist.v1.dto';
+import { EducationLevelModel } from 'src/models/educationlavel';
+import { EducationLevelDto } from './dto/educationlevel.v1.dto';
+import { TypeModel } from 'src/models/type';
+import { TypeDto } from './dto/type.v1.dto';
 import { AcademicPositionModel } from 'src/models/academicposition';
 import { AcademicPositionDto } from './dto/academicposition.v1.dto';
 
@@ -19,6 +19,7 @@ export class MasterDataFactoryV1 {
   mapMajorPositionModelsToDtos(data: MajorPositionModel[]): MajorPositionDto[] {
     return data.map((item) => this.mapMajorPositionModelToDto(item));
   }
+
   mapMajorPositionModelToDto(data: MajorPositionModel): MajorPositionDto {
     return {
       id: data.id,
