60dd6dfa74692e53f794a968539a1b55c28a2841
Sukum <sukumpat2547@gmail.com>
2025-08-19 23:54:29 +0700

    fix error

--- src/modules/masterdata/masterdata.factory.v1.ts
+++ src/modules/masterdata/masterdata.factory.v1.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { MajorPositionModel } from 'src/models/majorposition';
 import { MajorPositionDto } from './dto/majorposition.v1.dto';
+import { TypeDto } from './dto/type.v1.dto';
+import { TypeModel } from 'src/models/type';
 import { RoleModel } from 'src/models/role';
 import { RoleDto } from './dto/role.v1.dto';
-import { TypeCourseModel } from 'src/models/course';
-import { TypeCourseDto } from './dto/typecourse.v1.dto';
 import { ListTypeModel } from 'src/models/type';
 import { ListTypeDto } from './dto/typelist.v1.dto';
+import { AcademicPositionModel } from 'src/models/academicposition';
+import { AcademicPositionDto } from './dto/academicposition.v1.dto';
 
 @Injectable()
 export class MasterDataFactoryV1 {
   mapMajorPositionModelsToDtos(data: MajorPositionModel[]): MajorPositionDto[] {
     return data.map((item) => this.mapMajorPositionModelToDto(item));
   }
+
   mapMajorPositionModelToDto(data: MajorPositionModel): MajorPositionDto {
     return {
       id: data.id,
@@ -21,6 +24,21 @@ export class MasterDataFactoryV1 {
       createdDate: data.createdDate,
       updatedDate: data.updatedDate,
     };
+  }
+  mapTypeModelsToDtos(data: TypeModel[]): TypeDto[] {
+    return data.map((item) => this.mapTypeModelToDto(item));
+  }
+
+  mapTypeModelToDto(data: TypeModel): TypeDto {
+    return {
+      id: data.id,
+      name: data.name,
+      createdDate: data.createdDate,
+      updatedDate: data.updatedDate,
+    };
+  }
+  mapRoleModelsToRoleDtos(data: RoleModel[]): RoleDto[] {
+    return data.map((item) => this.mapRoleModelToRoleDto(item));
   }
 
   mapListTypeModelsToListTypeDtos(data: ListTypeModel[]): ListTypeDto[] {
@@ -35,5 +53,23 @@ export class MasterDataFactoryV1 {
       updatedDate: data.updatedDate,
     };
     return dto;
+  }
+
+  mapAcademicPositionModelsToDtos(
+    data: AcademicPositionModel[],
+  ): AcademicPositionDto[] {
+    return data.map((item) => this.mapAcademicPositionModelToDto(item));
+  }
+
+  mapAcademicPositionModelToDto(
+    data: AcademicPositionModel,
+  ): AcademicPositionDto {
+    return {
+      id: data.id,
+      positionTh: data.positionTh,
+      positionEn: data.positionEn,
+      createdDate: data.createdDate,
+      updatedDate: data.updatedDate,
+    };
   }
 }
