af4980c0686bced96a1a36d4cf903bd7b8b75a9a
ZM <zmxidn@gmail.com>
2025-08-19 18:43:47 +0700

    feat: create Controller MasterData TypeList

--- src/modules/masterdata/masterdata.controller.v1.ts
+++ src/modules/masterdata/masterdata.controller.v1.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { Controller, Get, HttpStatus , Query} from '@nestjs/common';
 import { MasterdataService } from './masterdata.service';
 import { MasterDataFactoryV1 } from './masterdata.factory.v1';
 import { MajorPositionDto } from './dto/majorposition.v1.dto';
 import { success } from 'src/core/interceptors/response.helper';
+import { ListTypeDto } from './dto/typelist.v1.dto';
 
 @Controller({
   path: 'master-data',
@@ -20,5 +21,13 @@ export class MasterdataControllerV1 {
     const dto =
       this.masterDataFactory.mapMajorPositionModelsToDtos(majorPositions);
     return success<MajorPositionDto[]>(dto, HttpStatus.OK);
+  }
+
+  @Get('type/list')
+  async getListType(@Query('type') type: string) {
+    const types = await this.masterDataService.getListType(type);
+    const dto = 
+      this.masterDataFactory.mapListTypeModelsToListTypeDtos(types);
+    return success<ListTypeDto[]>(dto, HttpStatus.OK);
   }
 }
--- src/modules/masterdata/masterdata.factory.v1.ts
+++ src/modules/masterdata/masterdata.factory.v1.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { MajorPositionModel } from 'src/models/majorposition';
 import { MajorPositionDto } from './dto/majorposition.v1.dto';
+import { ListTypeModel } from 'src/models/type';
+import { ListTypeDto } from './dto/typelist.v1.dto';
 
 @Injectable()
 export class MasterDataFactoryV1 {
@@ -16,5 +18,19 @@ export class MasterDataFactoryV1 {
       createdDate: data.createdDate,
       updatedDate: data.updatedDate,
     };
+  }
+
+  mapListTypeModelsToListTypeDtos(data: ListTypeModel[]): ListTypeDto[] {
+    return data.map((item) => this.mapListTypeModelToListTypeDto(item));
+  }
+
+  mapListTypeModelToListTypeDto(data: ListTypeModel): ListTypeDto {
+    const dto: ListTypeDto = {
+      id: data.id,
+      name: data.name,
+      createdDate: data.createdDate,
+      updatedDate: data.updatedDate,
+    };
+    return dto;
   }
 }
