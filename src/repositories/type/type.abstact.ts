a3861bc47b102f3b81246081ea2c67aa1c6af83d
ZM <zmxidn@gmail.com>
2025-08-24 00:34:48 +0700

    feat: add getListTypeByName

--- src/repositories/type/type.abstact.ts
+++ src/repositories/type/type.abstact.ts
@@ -3,4 +3,5 @@ import { ListTypeModel, TypeModel } from 'src/models/type';
 export abstract class ITypeRepository {
   abstract getTypes(): Promise<TypeModel[]>;
   abstract getListTypes(type: string): Promise<ListTypeModel[]>;
+  abstract getListTypeByName( type: string) : Promise<ListTypeModel>;
 }
--- src/repositories/type/type.repository.ts
+++ src/repositories/type/type.repository.ts
@@ -3,6 +3,8 @@ import { ITypeRepository } from './type.abstact';
 import { PrismaService } from 'src/provider/database/prisma/prisma.service';
 import { TypeFactory } from './type.factory';
 import { ListTypeModel, TypeModel } from 'src/models/type';
+import { HttpException } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common';
 
 @Injectable()
 export class TypeRepository implements ITypeRepository {
@@ -26,5 +28,16 @@ export class TypeRepository implements ITypeRepository {
     });
 
     return this.typeFactory.mapListTypeEntitiesToListTypeModels(data);
+  }
+
+  async getListTypeByName(type: string): Promise<ListTypeModel> {
+      const data = await this.prisma.listType.findUnique(
+        {
+          where: {name: type},
+        }
+      );
+
+      if(!data) throw new HttpException(`type with  name ${type} not found`,HttpStatus.NOT_FOUND);
+      return this.typeFactory.mapListTypeEntityToListTypeModel(data)
   }
 }
