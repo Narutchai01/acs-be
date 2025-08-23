764393a9f8c281682cd1a8e019f0ccd863220be8
Narutchai <koonfluk2015@gmail.com>
2025-08-23 16:51:59 +0700

    Fix typo in TeachingAssignmentEntity class name and adjust formatting in professor.entity.ts

--- src/entities/professor.entity.ts
+++ src/entities/professor.entity.ts
@@ -1,17 +1,35 @@
-import { Professor } from "@prisma/client";
-import { UserEntity } from "./user.entity";
+import { Professor } from '@prisma/client';
+import { UserEntity } from './user.entity';
+import { TeachingAssignment } from '@prisma/client';
+import { CourseEntity } from './course.entity';
 
 export class ProfessorEntity implements Professor {
-    id: number;
-    userId: number;
-    academicPosition: number;
-    majorPosition: number;
-    profRoom: string;
-    fieldOffexpertise: string;
-    createdDate: Date;
-    updatedDate: Date;
-    deletedDate: Date | null;
-    createdBy: number;
-    updatedBy: number;
-    user: UserEntity;
+  id: number;
+  userId: number;
+  academicPosition: number;
+  majorPosition: number;
+  profRoom: string;
+  fieldOffexpertise: string;
+  createdDate: Date;
+  updatedDate: Date;
+  deletedDate: Date | null;
+  createdBy: number;
+  updatedBy: number;
+  user: UserEntity;
+}
+
+export class TeacghingAssignmentEntity implements TeachingAssignment {
+  id: number;
+  professorId: number;
+  courseId: number;
+  semesterId: number;
+  academicYearId: number;
+  createdDate: Date;
+  updatedDate: Date;
+  deletedDate: Date | null;
+  createdBy: number;
+  updatedBy: number;
+  professor: ProfessorEntity | null;
+  course: CourseEntity;
+  user?: UserEntity | null;
 }
