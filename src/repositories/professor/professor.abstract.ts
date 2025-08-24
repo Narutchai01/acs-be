33f692e156af24b5d43f7715f2e05b4fba63072c
Narutchai <koonfluk2015@gmail.com>
2025-08-24 16:36:45 +0700

    feat: refactor database schema and entities for professors, academic positions, and expert fields; remove unused professor repository

--- src/repositories/professor/professor.abstract.ts
+++ src/repositories/professor/professor.abstract.ts
@@ -1,15 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import {
-  CreateProfessorModel,
-  UpdateProfessorModel,
-  ProfessorModel,
-} from 'src/models/professor';
+import { CreateProfessorModel, ProfessorModel } from 'src/models/professor';
 
 @Injectable()
 export abstract class IProfessorRepository {
   abstract createProfessor(data: CreateProfessorModel): Promise<ProfessorModel>;
-  abstract updateProfessor(
-    id: number,
-    data: UpdateProfessorModel,
-  ): Promise<ProfessorModel>;
 }
