6c0ae9598c0a0c641283a2245927b21543415124
Narutchai <koonfluk2015@gmail.com>
2025-09-11 02:08:46 +0700

    feature: add disable eslint

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,6 +2,9 @@ import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthenticatedRequest } from 'src/models/auth'; // Adjust the import path as necessary
+
+// ** eslint-disable-next-line @typescript-eslint/no-unused-vars **
+
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
