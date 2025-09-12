f0c6a05a175dba827ee4beb2ca633697e2ec3e1a
Sukum <sukumpat2547@gmail.com>
2025-09-13 01:36:12 +0700

    Create getProfessors into Controller

--- src/modules/professor/professor.controller.ts
+++ src/modules/professor/professor.controller.ts
@@ -7,6 +7,8 @@ import {
   UploadedFile,
   UseGuards,
   UseInterceptors,
+  Get,
+  Query,
 } from '@nestjs/common';
 import { ProfessorService } from './professor.service';
 import { CreateProfessorDtoV1 } from './dto/create-professor.dto.v1';
@@ -16,6 +18,8 @@ import { AuthenticatedRequest } from 'src/models/auth';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { ProfessorFactory } from './professor.factory';
 import { ProfessorDtoV1 } from './dto/professor.dto.v1';
+import { QueryProfessorDto } from './dto/get-professors.dto';
+import { Pageable } from 'src/models';
 
 @Controller({
   path: 'professors',
@@ -44,5 +48,22 @@ export class ProfessorController {
     const dto =
       this.professorFactory.mapProfessorModelToProfessorDto(professor);
     return success<ProfessorDtoV1>(dto, HttpStatus.CREATED);
+  }
+
+  @Get()
+  async getProfessors(@Query() query: QueryProfessorDto) {
+    const professors = await this.professorService.getProfessors(query);
+    const dto = professors.rows.map((professor) =>
+      this.professorFactory.mapProfessorModelToProfessorDto(professor),
+    );
+    
+    const data = {
+      rows: dto,
+      totalRecords: professors.totalRecords,
+      page: professors.page,
+      pageSize: professors.pageSize,
+    };
+
+    return success<Pageable<ProfessorDtoV1>>(data, HttpStatus.OK);
   }
 }
