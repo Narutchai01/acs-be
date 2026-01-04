0ff9e20b9a72e2091bd93761da4b2fe7ed7590d4
Narutchai <koonfluk2015@gmail.com>
2026-01-04 12:19:35 +0700

    feat: add title field to Curriculum model and update related DTOs and services

--- src/modules/curriculum/dto/curriculum.dto.ts
+++ src/modules/curriculum/dto/curriculum.dto.ts
@@ -2,6 +2,7 @@ import { BaseDto } from 'src/models/dto/base.dto';
 
 export class CurriculumDto extends BaseDto {
   readonly id: number;
+  readonly title: string;
   readonly year: string;
   readonly fileUrl: string;
   readonly description: string;
